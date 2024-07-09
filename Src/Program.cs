d422c226ffce51a013fceab3a1e6b5ba28401b1d
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-07-09 11:58:50 +0300

    Register DI

--- Src/Program.cs
+++ Src/Program.cs
@@ -1,25 +1,21 @@
 using ChatDeepSeekCoderV2.Components;
+using ChatDeepSeekCoderV2.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents()
+    .Services.AddSingleton<AIModel>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    app.UseExceptionHandler("/Error", createScopeForErrors: true)
+        .UseHsts();
 
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
+app.UseHttpsRedirection()
+    .UseStaticFiles()
+    .UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
